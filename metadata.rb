005d542cf36db29c00d65f5f00b377c1771ccbe5
Roman Kapitonov <RomanKapitonov@coherentsolutions.com>
2015-10-06 21:40:20 +0300

    Add apt and apache dependencies.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,10 @@
 name             'web_server'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'RomanKapitonov'
+maintainer_email 'RomanKapitonov.dev@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures web_server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends "apt"
+depends "apache2"
